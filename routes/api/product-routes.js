124bfab49f113fb68e4a6cc411f65ff7bf2939d6
DerVerruckte <michael.heinhold@gmail.com>
2021-10-16 20:27:19 -0600

    added product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, Tag]
+  })
+  .then(categorydb => res.json(categorydb))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    where: {
+      id: req.params.id
+    },
+    include: [Category, Tag]
+  })
+  .then(categorydb => {
+    if(!categorydb){
+      res.status(404).json({ message: 'Item not found' });
+      return;
+    }
+    res.json(categorydb);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -91,6 +116,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categorydb => {
+    if(!categorydb){
+      res.status(404).json({ message: 'Item not found' });
+      return;
+    }
+    res.json(categorydb);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
